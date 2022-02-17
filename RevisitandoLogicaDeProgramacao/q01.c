0ab311b774fb78e0a2dc4e08239576351a5acc33
luizfelipetorres <fellipe116@gmail.com>
2022-02-17 01:08:57 -0300

    Exercicio 1 concluido

---
+++ RevisitandoLogicaDeProgramacao/q01.c
@@ -0,0 +1,11 @@
+/*
+Faça um programa que imprima o seu nome, sua matrícula, 
+e o seu semestre de ingresso no curso. Cada informação em uma linha.
+*/
+#include <stdio.h>
+
+int main(){
+    printf("Nome: Luiz Felipe Torres Farias\n");
+    printf("Matricula: 2021210056\n");
+    printf("Ingresso: 2021.2");
+}
