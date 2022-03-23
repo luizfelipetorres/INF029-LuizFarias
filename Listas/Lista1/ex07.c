f363b57939c0badb40eca8e32eab9faa9a5454a7
Luiz-FelipeFe62 <>
2022-03-23 23:14:10 +0000

    Alterações feitas em sala de aula

--- Listas/Lista1/ex07.c
+++ Listas/Lista1/ex07.c
@@ -7,13 +7,28 @@ Essa função deve ler três palavras do
 usuário e retornar as três palavras. 
 A função main deve imprimir essas três palavras.
 */
+#define QTD 3
+
+void ler3Palavras(char palavras[][QTD], int n){
+
+    for (int i = 0; i < QTD; i++){
+        printf("Escreva uma palavra: ");
+        scanf("%s", palavras[i]);
+    }
+}
 
 int main(){
 //  Variáveis
+    char vetor[0][QTD];
 
 //  Coletar entradas
+    ler3Palavras(vetor, QTD);
 
 //  Tratar dados
+    for (size_t i = 0; i < QTD; i++){
+        printf("Vc digitou %s.\n", vetor[i]);
+    }
+    
 
 //  Exibir saídas
 
